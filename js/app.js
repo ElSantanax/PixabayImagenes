cbfe86d244150673f117b6fe708ad99d6066ace0
ElSantanax <josesantanaserrano130598@gmail.com>
2025-07-22 09:34:30 -0500

    Mostrar resultado de consulta

--- js/app.js
+++ js/app.js
@@ -13,7 +13,6 @@ function validarFormulario(e) {
         mostrarAlerta('Agrega un termino de busqueda');
         return;
     }
-
     buscarImagenes(terminoBusqueda);
 }
 
@@ -43,5 +42,35 @@ function buscarImagenes(termino) {
 
     fetch(url)
         .then(respuesta => respuesta.json())
-        .then(resultado => console.log(resultado.hits))
+        .then(resultado => {
+            mostrarImagen(resultado.hits)
+        })
+}
+
+function mostrarImagen(imagenes) {
+
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+           <div class="w-1/2 md:w-1/4 lg:1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+                    <div class="p-4">
+                        <p class="font-bold">${likes} <span class="font-light">Me gustas</span></p>
+                        <p class="font-bold">${views} <span class="font-light">Vistas</span></p>
+
+                        <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                            href="${largeImageURL}" target="_blank" rel="noopener noreferrer">
+                            Ver Imagen
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
 }
